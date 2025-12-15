2ec68454b9713a4bb0710cc634b2360353bb5208
subsubl <subsubl@gmail.com>
2025-12-15 11:54:34 +0100

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,9 @@ export default defineNuxtConfig({
       titleTemplate: '%s | Optika Ljubljana',
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&family=Source+Sans+3:wght@300;400;600;700&display=swap&subset=latin-ext' },
-        { rel: 'stylesheet', href: 'https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.css' }
+        { rel: 'stylesheet', href: 'https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.css' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/icon.png' }
       ],
       script: [
         { src: 'https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.js', async: true }
